efafe30b607900e19fc897281d25fec031316ec1
Rick <1801210375@pku.edu.cn>
2019-01-18 18:10:25 +0800

    Add main activity.

---
+++ src/main/java/nugnikoll/memocard/act_main.java
@@ -0,0 +1,20 @@
+package nugnikoll.memocard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class act_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
+
