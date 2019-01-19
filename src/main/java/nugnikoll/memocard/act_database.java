9e54a37c78915e1d4f819bd4cecae9f36c420e10
Rick <1801210375@pku.edu.cn>
2019-01-19 14:01:14 +0800

    Add a input dialog.

--- src/main/java/nugnikoll/memocard/act_database.java
+++ src/main/java/nugnikoll/memocard/act_database.java
@@ -1,10 +1,14 @@
 package nugnikoll.memocard;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,6 +18,8 @@ import java.util.List;
  * A login screen that offers login via email/password.
  */
 public class act_database extends AppCompatActivity {
+
+    String text_input_database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,9 @@ public class act_database extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
+            case R.id.action_add:
+                create_database();
+                return true;
             case R.id.action_login:
                 Intent itt = new Intent(this, act_login.class);
 			    startActivityForResult(itt, 1);
@@ -40,6 +49,30 @@ public class act_database extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    protected void create_database(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create new database");
+
+        final EditText input = new EditText(this);
+        //input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                text_input_database = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
 
