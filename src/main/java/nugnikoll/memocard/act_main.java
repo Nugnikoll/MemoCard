ee2d0d86729a6f1a26a7ee086aa1d4bc0c0250a2
Rick <1801210375@pku.edu.cn>
2019-01-19 12:53:50 +0800

    Add buttons.

--- src/main/java/nugnikoll/memocard/act_main.java
+++ src/main/java/nugnikoll/memocard/act_main.java
@@ -1,7 +1,11 @@
 package nugnikoll.memocard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +19,27 @@ public class act_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onComposeAction(MenuItem mi) {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                Intent itt = new Intent(this, act_login.class);
+			    startActivityForResult(itt, 1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
